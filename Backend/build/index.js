0a6196f52ed2753148768af91778ce476aa77c88
Katherine Stehberg <kstehberg@gmail.com>
2024-09-20 12:44:36 -0300

    updated Login.jsx

--- Backend/build/index.js
+++ Backend/build/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Para usar Bootstrap en el proyecto
-import App from './App'; // Asegúrate de que la ruta a App.js sea correcta
-import { BrowserRouter as Router } from 'react-router-dom';
+"use strict";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+require("bootstrap/dist/css/bootstrap.min.css");
+var _App = _interopRequireDefault(require("../Src/App"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)), document.getElementById('root'));
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('¡Hola, mundo!');
+});
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log("Servidor escuchando en el puerto ".concat(PORT));
+});
