af55606cb6c18bdc841cafd0328240b515680617
Katherine Stehberg <kstehberg@gmail.com>
2024-09-20 14:48:51 -0300

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Esto asegura que los recursos se resuelvan correctamente en producción
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
     hmr: {
-      overlay: true, // Muestra errores de desarrollo en el overlay en la app
+      overlay: true, // Habilitar superposición de errores en el desarrollo
     },
   },
   build: {
@@ -17,9 +18,9 @@ export default defineConfig({
     },
   },
   esbuild: {
-    jsxInject: `import React from 'react'`, // Asegúrate de importar React automáticamente en archivos JSX
-    loader: 'jsx', // Cargador para procesar JSX
-    include: /src\/.*\.jsx?$/, // Procesar archivos .js y .jsx en el directorio src
-    exclude: /node_modules/, // Excluir la carpeta node_modules
+    jsxInject: `import React from 'react'`, // Importar React automáticamente
+    loader: 'jsx', // Configura el cargador para procesar JSX
+    include: /src\/.*\.jsx?$/, // Procesa archivos en src
+    exclude: /node_modules/, // Excluir node_modules
   },
 });
